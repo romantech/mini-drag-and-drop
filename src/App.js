b53a9547e05e8aa08a51e2c62285a0063bd4c639
ColorFilter <johan@romantech.net>
2023-05-13 17:48:27 +0900

    🚀 feat(package.json): add @vercel/analytics dependency ✨ feat(App.js): add Analytics component from @vercel/analytics/react to track user behavior The @vercel/analytics package is added as a dependency to the project. The Analytics component from the package is then imported and added to the App.js file to track user behavior.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components/macro';
+import { Analytics } from '@vercel/analytics/react';
 import Lists from './components/Lists';
 import theme from './styles/theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Analytics />
       <S.Wrapper>
         <Lists />
       </S.Wrapper>
